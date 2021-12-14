162bf1b53d99c3d517d6530204432e9c438b2051
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2021-12-14 21:42:35 +0000

    Update

--- Examples/SampleDemo/build.ps1
+++ Examples/SampleDemo/build.ps1
@@ -1,2 +1,2 @@
 Clear-Host
-D:/Documents/Source/BitMagic/BitMagic/bin/Release/net6.0/BitMagic.exe --bmasm-file .\\src\\main.bmasm --asm-object-file .\\src\\main.compiled.json --prg-file .\\sampledemo.prg compile emulate
+D:/Documents/Source/BitMagic/BitMagic/bin/Release/net6.0/BitMagic.exe --bmasm-file .\\src\\main.bmasm --asm-object-file .\\src\\main.compiled.json --output-file .\\sampledemo.prg --rom-file ..\\..\\Rom\\rom.bin compile emulate
