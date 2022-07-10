0772286d005f11f71321120f36c5503cd2bfbbdf
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2022-07-10 20:02:31 +0100

    STA Wrap Tests

--- BitMagic.X16Emulator.Tests/STA.cs
+++ BitMagic.X16Emulator.Tests/STA.cs
@@ -60,6 +60,31 @@ public class STA
     }
 
     [TestMethod]
+    public async Task ZeroPageX_Wrap()
+    {
+        var emulator = new Emulator();
+
+        emulator.A = 0x44;
+        emulator.X = 0x70;
+
+        await X16TestHelper.Emulate(@"                
+                .machine CommanderX16R40
+                .org $810
+                sta $a0, X
+                stp",
+                emulator);
+
+        // compilation
+        Assert.AreEqual(0x95, emulator.Memory[0x810]);
+        Assert.AreEqual(0xa0, emulator.Memory[0x811]);
+
+        // emulation
+        Assert.AreEqual(0x44, emulator.Memory[0x10]);
+        emulator.AssertState(0x44, 0x70, 0x00, 0x813);
+        emulator.AssertFlags(false, false, false, false);
+    }
+
+    [TestMethod]
     public async Task Absolute()
     {
         var emulator = new Emulator();
@@ -161,6 +186,34 @@ public class STA
         // emulation
         Assert.AreEqual(0x44, emulator.Memory[0x105]);
         emulator.AssertState(0x44, 0x10, 0x00, 0x813);
+        emulator.AssertFlags(false, false, false, false);
+    }
+
+    [TestMethod]
+    public async Task IndirectX_Wrap()
+    {
+        var emulator = new Emulator();
+
+        emulator.A = 0x44;
+        emulator.X = 0x70;
+
+        emulator.Memory[0x10] = 0x05;
+        emulator.Memory[0x11] = 0x01;
+
+        await X16TestHelper.Emulate(@"                
+                .machine CommanderX16R40
+                .org $810
+                sta ($a0, X)
+                stp",
+                emulator);
+
+        // compilation
+        Assert.AreEqual(0x81, emulator.Memory[0x810]);
+        Assert.AreEqual(0xa0, emulator.Memory[0x811]);
+
+        // emulation
+        Assert.AreEqual(0x44, emulator.Memory[0x105]);
+        emulator.AssertState(0x44, 0x70, 0x00, 0x813);
         emulator.AssertFlags(false, false, false, false);
     }
 
