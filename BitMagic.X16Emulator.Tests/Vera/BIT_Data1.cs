726b4c085035cc26f4a89866f4aef8d5982c2cd5
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2022-08-19 16:22:23 +0100

    BIT Data1

---
+++ BitMagic.X16Emulator.Tests/Vera/BIT_Data1.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitMagic.X16Emulator.Tests;
+
+[TestClass]
+public class BIT_Data1
+{
+    [TestMethod]
+    public async Task Abs_Step0()
+    {
+        var emulator = new Emulator();
+
+        emulator.Vera.Data1_Step = 0;
+        emulator.Vera.Data1_Address = 0x0000;
+        emulator.Vera.Vram[0x0000] = 0xee;
+        emulator.Vera.Vram[0x0002] = 0xff;
+        emulator.A = 0xff;
+        emulator.Vera.AddrSel = true;
+
+        await X16TestHelper.Emulate(@"
+                .machine CommanderX16R40
+                .org $810
+                bit DATA1
+                stp",
+                emulator);
+
+        Assert.AreEqual(true, emulator.Negative);
+
+        Assert.AreEqual(0x00000, emulator.Vera.Data1_Address);
+        Assert.AreEqual(0xee, emulator.Memory[0x9F24]);
+
+        Assert.AreEqual(0x00, emulator.Memory[0x9F20]);
+        Assert.AreEqual(0x00, emulator.Memory[0x9F21]);
+        Assert.AreEqual(0x00, emulator.Memory[0x9F22]);
+    }
+
+    [TestMethod]
+    public async Task Abs_Step1()
+    {
+        var emulator = new Emulator();
+
+        emulator.Vera.Data1_Step = 1;
+        emulator.Vera.Data1_Address = 0x0000;
+        emulator.Vera.Vram[0x0000] = 0xee;
+        emulator.Vera.Vram[0x0001] = 0xff;
+        emulator.A = 0xff;
+        emulator.Vera.AddrSel = true;
+
+        await X16TestHelper.Emulate(@"
+                .machine CommanderX16R40
+                .org $810
+                bit DATA1
+                stp",
+                emulator);
+
+        Assert.AreEqual(true, emulator.Negative);
+
+        Assert.AreEqual(0x00001, emulator.Vera.Data1_Address);
+        Assert.AreEqual(0xff, emulator.Memory[0x9F24]);
+
+        Assert.AreEqual(0x01, emulator.Memory[0x9F20]);
+        Assert.AreEqual(0x00, emulator.Memory[0x9F21]);
+        Assert.AreEqual(0x10, emulator.Memory[0x9F22]);
+    }
+
+    [TestMethod]
+    public async Task AbsX_Step0()
+    {
+        var emulator = new Emulator();
+
+        emulator.Vera.Data1_Step = 0;
+        emulator.Vera.Data1_Address = 0x0000;
+        emulator.Vera.Vram[0x0000] = 0xee;
+        emulator.Vera.Vram[0x0002] = 0xff;
+        emulator.A = 0xff;
+        emulator.X = 0x24;
+        emulator.Vera.AddrSel = true;
+
+        await X16TestHelper.Emulate(@"
+                .machine CommanderX16R40
+                .org $810
+                bit $9f00, x
+                stp",
+                emulator);
+
+        Assert.AreEqual(true, emulator.Negative);
+
+        Assert.AreEqual(0x00000, emulator.Vera.Data1_Address);
+        Assert.AreEqual(0xee, emulator.Memory[0x9F24]);
+
+        Assert.AreEqual(0x00, emulator.Memory[0x9F20]);
+        Assert.AreEqual(0x00, emulator.Memory[0x9F21]);
+        Assert.AreEqual(0x00, emulator.Memory[0x9F22]);
+    }
+
+    [TestMethod]
+    public async Task AbsX_Step1()
+    {
+        var emulator = new Emulator();
+
+        emulator.Vera.Data1_Step = 1;
+        emulator.Vera.Data1_Address = 0x0000;
+        emulator.Vera.Vram[0x0000] = 0xee;
+        emulator.Vera.Vram[0x0001] = 0xff;
+        emulator.A = 0xff;
+        emulator.X = 0x24;
+        emulator.Vera.AddrSel = true;
+
+        await X16TestHelper.Emulate(@"
+                .machine CommanderX16R40
+                .org $810
+                bit $9f00, x
+                stp",
+                emulator);
+
+        Assert.AreEqual(true, emulator.Negative);
+
+        Assert.AreEqual(0x00001, emulator.Vera.Data1_Address);
+        Assert.AreEqual(0xff, emulator.Memory[0x9F24]);
+
+        Assert.AreEqual(0x01, emulator.Memory[0x9F20]);
+        Assert.AreEqual(0x00, emulator.Memory[0x9F21]);
+        Assert.AreEqual(0x10, emulator.Memory[0x9F22]);
+    }
+}
