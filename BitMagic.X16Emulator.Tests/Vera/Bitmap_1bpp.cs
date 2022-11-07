302ee299e1bc2b4ae7fd00c37c645c7f27f2b325
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2022-11-07 21:18:13 +0000

    Bitmap 1bpp unit test

---
+++ BitMagic.X16Emulator.Tests/Vera/Bitmap_1bpp.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitMagic.X16Emulator.Tests.Vera.Display;
+
+[TestClass]
+public class Bitmap_1Bpp
+{
+    [TestMethod]
+    public async Task Normal_Layer0()
+    {
+        var emulator = new Emulator();
+
+        emulator.LoadImage(@"Vera\Images\testimage_1bpp.png", ImageHelper.ColourDepth.Depth_1bpp, 0);
+
+        await X16TestHelper.Emulate(@$"
+                    .machine CommanderX16R40
+                    .org $810
+                        sei
+
+                        lda #02
+                        sta DC_BORDER
+
+                        lda #$11        ; enable layer 0
+                        sta DC_VIDEO
+
+                        lda #$04        ; bitmap, 1bpp
+                        sta L0_CONFIG
+
+                        lda #$01        ; 640 wide
+                        sta L0_TILEBASE 
+
+                        ; set colour 0
+                        lda #$01
+                        sta ADDRx_H
+                        lda #$fa
+                        sta ADDRx_M
+                        stz ADDRx_L
+
+                        lda #$12
+                        sta DATA0
+
+                        ldx #128
+                        stx DC_VSCALE  
+                        stx DC_HSCALE
+
+                        lda #01
+                        sta IEN
+                        wai
+                        sta ISR     ; clear interrupt and wait for second frame
+                        wai
+
+                        stp
+
+                    .proc blankline
+                        ldx #$50
+
+                    .loop:
+                        stz DATA0
+                        dex
+                        bne loop
+
+                        rts
+                    .endproc
+
+                    .proc fullline
+                        lda #$7f
+                        sta DATA0
+                        ldx #$4e
+                        lda #$ff
+                    .loop:
+                        sta DATA0
+                        dex
+                        bne loop
+
+                        lda #$fe
+                        sta DATA0
+
+                        rts
+                    .endproc
+
+                    .proc middleline
+                        lda #$40
+                        sta DATA0
+                        ldx #$4e
+                    .loop:
+                        stz DATA0
+                        dex
+                        bne loop
+
+                        lda #$02
+                        sta DATA0
+
+                        rts
+                    .endproc
+                    ",
+                emulator);
+
+        //emulator.SaveDisplay(@"D:\Documents\Source\BitMagic\BitMagic.X16Emulator.Tests\Vera\Images\bitmap_1bpp_l0_normal.png");
+        emulator.CompareImage(@"Vera\Images\bitmap_1bpp_l0_normal.png");
+    }
+
+}
---
+++ BitMagic.X16Emulator.Tests/Vera/Bitmap_Helper.cs
@@ -0,0 +1,95 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Advanced;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitMagic.X16Emulator.Tests.Vera
+{
+    internal static class ImageHelper
+    {
+        public enum ColourDepth
+        {
+            Depth_1bpp,
+            Depth_2bpp,
+            Depth_4bpp,
+            Depth_8bpp
+        }
+
+        public static void LoadImage(this Emulator emulator, string filename, ColourDepth depth, int address)
+        {
+            var sb = new StringBuilder();
+
+            using var image = Image.Load<Rgba32>(filename);
+
+            if (image.Height != 480)
+                Assert.Fail("Source image must be 480px high.");
+
+            if (image.Width != 640)
+                Assert.Fail("Source image must be 640px wide.");
+
+            var counter = 1;
+            var colourIndex = new Dictionary<Rgba32, int>();
+            colourIndex.Add(new Rgba32(0, 0, 0, 255), 0);
+
+            var maxColours = depth switch {
+                ColourDepth.Depth_1bpp => 2,
+                ColourDepth.Depth_2bpp => 4,
+                ColourDepth.Depth_4bpp => 16,
+                ColourDepth.Depth_8bpp => 256,
+                _ => throw new Exception("Unknown depth")
+            };
+
+            var maxSubCount = depth switch
+            {
+                ColourDepth.Depth_1bpp => 8,
+                ColourDepth.Depth_2bpp => 4,
+                ColourDepth.Depth_4bpp => 2,
+                ColourDepth.Depth_8bpp => 1,
+                _ => throw new Exception("Unknown depth")
+            };
+
+            var dataCount = 0;
+            sb.Append(".byte ");
+            image.ProcessPixelRows(accessor =>
+            {
+                for (var y = 0; y < 480; y++)
+                {
+                    var rowSpan = accessor.GetRowSpan(y);
+
+                    byte thisEntry = 0;
+                    int subByteCount = maxSubCount;
+
+                    for (var x = 0; x < 640; x++)
+                    {
+                        var pixel = rowSpan[x];
+
+                        if (!colourIndex.ContainsKey(pixel))
+                        {
+                            colourIndex.Add(pixel, counter++);
+                            if (colourIndex.Count > maxColours)
+                                throw new Exception("Too many colours.");
+                        }
+
+                        var index = colourIndex[pixel];
+
+                        subByteCount--;
+
+                        thisEntry |= (byte)(index << subByteCount);
+
+                        if (subByteCount == 0)
+                        {
+                            emulator.Vera.Vram[address++] = thisEntry;
+                            thisEntry = 0;
+                            subByteCount = maxSubCount;
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
